5eed7621ce8f17762d3b6a72cd6f1efce60e43f0
ahqcode <332252453@qq.com>
2018-12-28 22:09:21 +0800

    设备管理

--- city-fire-front/src/store/modules/common.js
+++ city-fire-front/src/store/modules/common.js
@@ -4,9 +4,9 @@ export default {
     // 页面文档可视高度(随窗口改变大小)
     documentClientHeight: 0,
     // 导航条, 布局风格, defalut(默认) / inverse(反向)
-    navbarLayoutType: 'default',
+    navbarLayoutType: 'inverse',
     // 侧边栏, 布局皮肤, light(浅色) / dark(黑色)
-    sidebarLayoutSkin: 'dark',
+    sidebarLayoutSkin: 'light',
     // 侧边栏, 折叠状态
     sidebarFold: false,
     // 侧边栏, 菜单
---
+++ city-fire-front/src/views/modules/equipment/add.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="mod-menu">
+    <el-form :inline="true" label-width="150px">
+      <el-form-item label="设备名称：">
+        <el-input v-model="dataForm.province" placeholder="输入设备名称" ></el-input>
+      </el-form-item>
+      <el-form-item label="选择分类：">
+        <el-input v-model="dataForm.city" readonly="readonly">
+        </el-input>
+      </el-form-item>
+      <el-form-item label="数量(件)：">
+        <el-input v-model="dataForm.citycode" placeholder="输入设备数量"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-form label-width="150px">
+      <el-form-item label="选择设备位置：">
+        <el-input v-model="dataForm.locationName" readonly="readonly"  style="width:535px"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-form label-width="150px">
+      <el-form-item label="确认设备编码：">
+        <el-input v-model="dataForm.locationName" readonly="readonly" style="width:535px"></el-input>
+        <el-button type="primary">生成编码</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-form :inline="true" label-width="150px">
+      <el-form-item label="小区：">
+        <el-input v-model="dataForm.district" readonly="readonly"></el-input>
+      </el-form-item>
+      <el-form-item label="单元：">
+        <el-input v-model="dataForm.adcode" readonly="readonly"></el-input>
+      </el-form-item>
+      <el-form-item label="楼层：">
+        <el-input v-model="dataForm.street" readonly="readonly"></el-input>
+      </el-form-item>
+      <el-form-item label="楼道：">
+        <el-input v-model="dataForm.streetNumber" readonly="readonly"></el-input>
+      </el-form-item>
+      <el-form-item label="房间号">
+        <el-input v-model="dataForm.lat" readonly="readonly"></el-input>
+      </el-form-item>
+      <el-form-item label="管理员：">
+        <el-input v-model="dataForm.lng" readonly="readonly"></el-input>
+      </el-form-item>
+      <el-form-item label="联系电话：">
+        <el-input v-model="dataForm.lng" readonly="readonly"></el-input>
+      </el-form-item>
+    </el-form>
+    <el-form label-width="150px">
+      <el-form-item label="备注信息：">
+        <el-input
+          type="textarea"
+          :autosize="{ minRows: 2, maxRows: 4}"
+          placeholder="请输入内容"
+          v-model="dataForm.remark" ></el-input>
+      </el-form-item>
+    </el-form>
+    <div >
+      <el-button type="primary" style="margin: 0 auto" @click="updateRemark()">确定</el-button>
+    </div>
+    <!-- 弹窗, 新增 / 修改 -->
+  <!--  <ShowLocationInfo v-if="showLocationInfo" ref="showLocationInfo" @refreshDataList="getDataList"></ShowLocationInfo>
+-->
+  </div>
+</template>
+
+<script>
+ // import ShowLocationInfo from  './location-info'
+  export default {
+    data () {
+      return {
+        dataForm: {
+          locationId:'',
+          locationName:'',
+          province: '',
+          city: '',
+          citycode: '',
+          district: '',
+          adcode: '',
+          street: '',
+          streetNumber: '',
+          lat:'',
+          lng:'',
+          status:'',
+          gmtCreate:''
+        },
+        dataList: [
+          {
+            locationId:'',
+            locationName:'',
+            province: '',
+            city: '',
+            citycode: '',
+            district: '',
+            adcode: '',
+            street: '',
+            streetNumber: '',
+            lat:'',
+            lng:'',
+            status:'',
+            gmtCreate:''
+          }
+        ],
+        dataListLoading: false,
+        addOrUpdateVisible: false,
+        showLocationInfo:false
+      }
+    },
+    components: {
+    },
+    activated () {
+      this.getDataList()
+    },
+    methods: {
+      // 获取数据列表
+      getDataList () {
+        this.dataListLoading = true
+        this.$http({
+          url: this.$http.adornUrl('/location/list'),
+          method: 'get',
+          params: this.$http.adornParams()
+        }).then(({data}) => {
+          this.dataList = data.page.list
+          this.dataListLoading = false
+        })
+      },
+      showinfor(locationId){
+        this.dataListLoading = true
+        this.$http({
+          url: this.$http.adornUrl(`/location/info/${locationId}`),
+          method: 'get',
+          params: this.$http.adornParams()
+        }).then(({data}) => {
+          this.dataForm = data.cfLocation
+          this.dataListLoading = false
+          this.showLocationInfo =true
+          this.$nextTick(() => {
+            this.$refs.showLocationInfo.init(this.dataForm)
+          })
+        })
+      },
+      // 新增 / 修改
+      addOrUpdateHandle (id) {
+        this.addOrUpdateVisible = true
+      },
+      //禁用
+      stopUse(id,status){
+        let self = this
+        self.$http({
+          url:this.$http.adornUrl(`/location/updateStatus`),
+          method:'post',
+          params:this.$http.adornParams({
+            locationid:id,
+            status:status
+          })
+        }).then(({data}) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList()
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          }
+        )
+      },
+      // 删除
+      deleteHandle (id) {
+        var ids = new Array()
+        ids[0] = id
+        this.$confirm(`确定对其进行【删除】操作?`, '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.$http({
+            url: this.$http.adornUrl(`/location/delete`),
+            method: 'post',
+            data: this.$http.adornData(ids,false)
+          }).then(({data}) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.getDataList()
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        }).catch(() => {})
+      }
+    }
+  }
+</script>
