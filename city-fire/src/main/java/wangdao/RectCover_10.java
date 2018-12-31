70e05196ecf0f79a8434d07c54e547d7209e0242
ahqcode <332252453@qq.com>
2018-12-31 23:33:54 +0800

    今天刷了4道，都是关于斐波拉契数列 计算F(n)的

---
+++ city-fire/src/main/java/wangdao/RectCover_10.java
@@ -0,0 +1,23 @@
+package wangdao;
+
+public class RectCover_10 {
+    public int RectCover(int target) {
+        int result = 0;
+        int before1 = 2,before2=1;
+        if (target == 0){
+            return 0;
+        }
+        if (target == 1){
+            return before2;
+        }
+        if (target ==2){
+            return before1;
+        }
+        for (int i =3;i <= target;i++){
+            result = before2+before1;
+            before2 = before1;
+            before1 = result;
+        }
+        return result;
+    }
+}
