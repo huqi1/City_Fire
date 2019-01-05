ea5d069eb2795483080a8ac28edff3b861c3a7d0
ahqcode <332252453@qq.com>
2019-01-05 23:44:24 +0800

    新增了一些关于设备信息的字段

--- city-fire/src/main/java/com/hq/modules/operate/entity/EquipmentEntity.java
+++ city-fire/src/main/java/com/hq/modules/operate/entity/EquipmentEntity.java
@@ -12,6 +12,7 @@ import java.util.Date;
  * @author hq
  * @date 2018-12-17 09:25:46
  */
+
 @TableName("cf_equipment")
 public class EquipmentEntity implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -57,6 +58,36 @@ public class EquipmentEntity implements Serializable {
 	 * 设备状态: 0:在库 1：检修 2：在用
 	 */
 	private Integer locationStatus;
+
+	/**
+	 * 小区
+	 */
+	private String community;
+	/**
+	 * 单元
+	 */
+	private String unit;
+	/**
+	 * 楼层
+	 */
+	private String floor;
+	/**
+	 * 楼道
+	 */
+	private String corridor;
+	/**
+	 * 房间号
+	 */
+	private String roomNumber;
+	/**
+	 * 管理员
+	 */
+	private String administrator;
+	/**
+	 * 联系电话
+	 */
+	private String phone;
+
 	/**
 	 * 创建时间
 	 */
@@ -68,130 +99,130 @@ public class EquipmentEntity implements Serializable {
 	public void setEquipmentId(String equipmentId) {
 		this.equipmentId = equipmentId;
 	}
-	/**
-	 * 获取：
-	 */
-	public String getEquipmentId() {
-		return equipmentId;
-	}
+
 	/**
 	 * 设置：设备名称
 	 */
 	public void setEquipmentName(String equipmentName) {
 		this.equipmentName = equipmentName;
 	}
-	/**
-	 * 获取：设备名称
-	 */
-	public String getEquipmentName() {
-		return equipmentName;
-	}
+
 	/**
 	 * 设置：所属分类id
 	 */
 	public void setBelongTypeid(String belongTypeid) {
 		this.belongTypeid = belongTypeid;
 	}
-	/**
-	 * 获取：所属分类id
-	 */
-	public String getBelongTypeid() {
-		return belongTypeid;
-	}
+
 	/**
 	 * 设置：所属分类名称
 	 */
 	public void setBelongTypename(String belongTypename) {
 		this.belongTypename = belongTypename;
 	}
-	/**
-	 * 获取：所属分类名称
-	 */
-	public String getBelongTypename() {
-		return belongTypename;
-	}
+
 	/**
 	 * 设置：设备价格
 	 */
 	public void setEquipmentPrice(Integer equipmentPrice) {
 		this.equipmentPrice = equipmentPrice;
 	}
-	/**
-	 * 获取：设备价格
-	 */
-	public Integer getEquipmentPrice() {
-		return equipmentPrice;
-	}
+
 	/**
 	 * 设置：设备数量
 	 */
 	public void setEquipmentNum(Integer equipmentNum) {
 		this.equipmentNum = equipmentNum;
 	}
-	/**
-	 * 获取：设备数量
-	 */
-	public Integer getEquipmentNum() {
-		return equipmentNum;
-	}
+
 	/**
 	 * 设置：设备所在位置id
 	 */
 	public void setLocaltionId(String localtionId) {
 		this.localtionId = localtionId;
 	}
-	/**
-	 * 获取：设备所在位置id
-	 */
-	public String getLocaltionId() {
-		return localtionId;
-	}
+
 	/**
 	 * 设置：设备所在位置名称
 	 */
 	public void setLocationName(String locationName) {
 		this.locationName = locationName;
 	}
-	/**
-	 * 获取：设备所在位置名称
-	 */
-	public String getLocationName() {
-		return locationName;
-	}
+
 	/**
 	 * 设置：设备状态0:正常 1：报修 2：检修 3：报废
 	 */
 	public void setEquipmentStatus(Integer equipmentStatus) {
 		this.equipmentStatus = equipmentStatus;
 	}
-	/**
-	 * 获取：设备状态0:正常 1：报修 2：检修 3：报废
-	 */
-	public Integer getEquipmentStatus() {
-		return equipmentStatus;
-	}
+
 	/**
 	 * 设置：设备状态: 0:在库 1：检修 2：在用
 	 */
 	public void setLocationStatus(Integer locationStatus) {
 		this.locationStatus = locationStatus;
 	}
-	/**
-	 * 获取：设备状态: 0:在库 1：检修 2：在用
-	 */
-	public Integer getLocationStatus() {
-		return locationStatus;
-	}
+
 	/**
 	 * 设置：创建时间
 	 */
 	public void setGmtCreate(Date gmtCreate) {
 		this.gmtCreate = gmtCreate;
 	}
-	/**
-	 * 获取：创建时间
-	 */
-	public Date getGmtCreate() {
-		return gmtCreate;
+
+	public String getCommunity() {
+		return community;
+	}
+
+	public void setCommunity(String community) {
+		this.community = community;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getFloor() {
+		return floor;
+	}
+
+	public void setFloor(String floor) {
+		this.floor = floor;
+	}
+
+	public String getCorridor() {
+		return corridor;
+	}
+
+	public void setCorridor(String corridor) {
+		this.corridor = corridor;
+	}
+
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+
+	public String getAdministrator() {
+		return administrator;
+	}
+
+	public void setAdministrator(String administrator) {
+		this.administrator = administrator;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
 	}
 }
